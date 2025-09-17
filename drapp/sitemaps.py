a60910bc04418331db4f5d5e7a7cd66359ba8713
unknown <khaledgaber494@gmail.com>
2025-09-18 02:21:07 +0300

    seo

--- drapp/sitemaps.py
+++ drapp/sitemaps.py
@@ -1,6 +1,5 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import YourModel  # لو عندك موديلات للصفحات الديناميكية
 
 class StaticViewSitemap(Sitemap):
     priority = 0.8
@@ -13,12 +12,4 @@ class StaticViewSitemap(Sitemap):
     def location(self, item):
         return reverse(item)
 
-class BlogSitemap(Sitemap):
-    priority = 0.6
-    changefreq = 'weekly'
 
-    def items(self):
-        return YourModel.objects.all()  # لو عندك مقالات أو صفحات ديناميكية
-
-    def lastmod(self, obj):
-        return obj.updated_at  # حقل تاريخ آخر تعديل في الموديل
